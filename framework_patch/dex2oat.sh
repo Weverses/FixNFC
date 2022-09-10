797157569bbe0545f1c7bced91fbac6577e179bc
weverse <2364908189@qq.com>
2022-09-10 17:32:46 +0800

    Fix dex2oat.sh

--- framework_patch/dex2oat.sh
+++ framework_patch/dex2oat.sh
@@ -1,2 +1,4 @@
+mkdir -p /sdcard/framework/oat/arm64/
+mkdir -p /sdcard/framework/oat/arm/
 dex2oat --dex-file=/sdcard/framework/framework.jar --compiler-filter=everything --instruction-set=arm64 --oat-file=/sdcard/framework/oat/arm64/framework.odex
 dex2oat --dex-file=/sdcard/framework/framework.jar --compiler-filter=everything --instruction-set=arm --oat-file=/sdcard/framework/oat/arm/framework.odex
